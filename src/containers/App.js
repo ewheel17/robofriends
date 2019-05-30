9ddb2ffaf8c5fac8315664e47f93dfeacc59bd90
elliott max wheeler <ewheel17@gmail.com>
2019-05-30 10:00:54 -0600

    performance upgrade wrt components

--- src/containers/App.js
+++ src/containers/App.js
@@ -3,6 +3,7 @@ import { connect } from 'react-redux';
 import CardList from '../components/CardList';
 import SearchBox from '../components/SearchBox';
 import Scroll from '../components/Scroll';
+import Header from '../components/Header';
 import ErrorBoundary from '../components/ErrorBoundary';
 import './App.css';
 import { setSearchField, requestRobots } from '../actions';
@@ -40,7 +41,7 @@ class App extends Component {
     } else {
       return (
         <div className='tc'>
-          <h1 className='f1'>RoboFriends</h1>
+          <Header />
           <SearchBox searchChange={onSearchChange}/>
           <Scroll>
             <ErrorBoundary>

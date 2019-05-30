015cf141bc574bc817d1345a5b0d21db6b470520
elliott max wheeler <ewheel17@gmail.com>
2019-05-30 15:26:30 -0600

    PWA enhancements

--- src/components/SearchBox.js
+++ src/components/SearchBox.js
@@ -8,6 +8,7 @@ function SearchBox({ searchField, searchChange }) {
         placeholder='Search Robots'
         onChange={ searchChange }
         className='pa3 ba b--green bg-lightest-blue'
+        aria-label="Search Robots"
       />
     </div>
   )

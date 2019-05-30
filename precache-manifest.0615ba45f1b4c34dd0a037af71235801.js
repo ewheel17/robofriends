40fba7c87e992e4f8f6070ece2fe761ff4732568
elliott max wheeler <ewheel17@gmail.com>
2019-05-30 10:29:59 -0600

    Updates

---
+++ precache-manifest.0615ba45f1b4c34dd0a037af71235801.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e677587409ec71f00fd9142ad34375b0",
+    "url": "/robofriends/index.html"
+  },
+  {
+    "revision": "8d8ad546c8bba8b27b11",
+    "url": "/robofriends/static/css/2.31dafeb2.chunk.css"
+  },
+  {
+    "revision": "24075bbd82a2df8cd610",
+    "url": "/robofriends/static/css/main.51dd40e3.chunk.css"
+  },
+  {
+    "revision": "8d8ad546c8bba8b27b11",
+    "url": "/robofriends/static/js/2.c05ccf0c.chunk.js"
+  },
+  {
+    "revision": "24075bbd82a2df8cd610",
+    "url": "/robofriends/static/js/main.0eb05e48.chunk.js"
+  },
+  {
+    "revision": "143eb19aa518302daf32",
+    "url": "/robofriends/static/js/runtime~main.fb41d28d.js"
+  },
+  {
+    "revision": "12a3db8ba7f5423327bb12aed3c41977",
+    "url": "/robofriends/static/media/SEGA.12a3db8b.woff"
+  }
+]);
